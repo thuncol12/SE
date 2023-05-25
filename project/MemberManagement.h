a12428e6524fa3635b2fc83d066b84f756b0a0df
Jaeho-99 <wogh6860@gmail.com>
2023-05-26 01:16:47 +0900

    주석처리 완료

--- project/MemberManagement.h
+++ project/MemberManagement.h
@@ -18,13 +18,13 @@ class MemberManagement {
 public: 
     MemberManagement();
 
-bool checkID(Member* mem, string id, string password);
+bool checkID(Member* mem, string id, string password); // mem이 맞는지 체크한다.
         
-void logOut(Member* mem);
+void logOut(Member* mem); // mem이 로그아웃을 선택하면 해당 함수가 호출된다.
         
-void signOut(Member* mem, MemberList memList);
+void signOut(Member* mem, MemberList memList); // 회원이 회원탈퇴를 선택하면 해당 함수가 호출된다. 이때, 매개변수로 멤버 주소와 시스템이 갖고 있는 멤버리스트를 넘겨준다.
         
-void signUp(int flag, string name, int number, string id, string password, MemberList memList);
+void signUp(int flag, string name, int number, string id, string password, MemberList memList); // 비회원이 회원가입을 선택하면 해당 함수가 호출된다. flag: 1은 회사회원, 2는 일반회원, numer: 사업자 혹은 주민번호, memList: 시스템의 멤버리스트에 해당 회원 추가를 위해 전달.
 };
 
 #endif //_MEMBERMANAGEMENT_H
--- project/Recruitment.cpp
+++ project/Recruitment.cpp
@@ -8,6 +8,8 @@
 /**
  * Recruitment implementation
  */
+
+// 채용 생성자, part: 업무, numOfDesired: 마감인원 수, deadline: 마감일, companyName: 회사이름, businessNumber: 사업자번호
 Recruitment::Recruitment(string part, int numOfDesired, string deadline, string companyName, int businessNumber) {
     this->part = part;
     this->numOfDesired = numOfDesired;
@@ -19,6 +21,8 @@ Recruitment::Recruitment(string part, int numOfDesired, string deadline, string 
 /**
  * @return Recruitment
  */
+
+// 채용 정보 리턴
 string Recruitment::getRecruitmentInfo() {
     string info = "";
     info = this->part + " " + to_string(this->numOfDesired) + " " + this->deadline; // info = 업무 인원 수 마감일
